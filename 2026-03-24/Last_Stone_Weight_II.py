a2e286ac5ca04fb40ee700af6eeb9d116aaa3948
Lakshman Siva <slakshman664@gmail.com>
2026-03-25 00:00:05 -0400

    chore(leetcode): automated push of today's solved problems using script

--- 2026-03-24/Last_Stone_Weight_II.py
+++ 2026-03-24/Last_Stone_Weight_II.py
@@ -1,20 +1,27 @@
+from functools import cache
 from typing import List
 
 
 class Solution:
-    def lastStoneWeightII2(self, stones: List[int]) -> int:
-        n = len(stones)
-        MAX = (1 << 31) - 1
-        dp = [[MAX] * (n+1) for _ in range(n+1)]
-
-
     def lastStoneWeightII(self, stones: List[int]) -> int:
+        total = sum(stones)
+        target = total // 2
+        dp = [0] * (target + 1)
+
+        for s in stones:
+            for t in range(target, s-1, -1):
+                dp[t] = max(dp[t], dp[t - s] + s)
+
+        return total - 2 * dp[target]
+
+    def lastStoneWeightII2(self, stones: List[int]) -> int:
         total = sum(stones)
         target = (total + 1) // 2
 
+        @cache
         def dfs(i, cur_sum):
             if i < 0 or cur_sum >= target:
-                return abs(cur_sum - (total - cur_sum))
+                return abs(2 * cur_sum - total)
 
             return min(dfs(i-1, cur_sum), dfs(i-1, cur_sum + stones[i]))
 
