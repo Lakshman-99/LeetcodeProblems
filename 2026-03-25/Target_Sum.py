3ea0862ede60481f5e0b9462da683a547fe00130
Lakshman Siva <slakshman664@gmail.com>
2026-03-26 00:00:04 -0400

    chore(leetcode): automated push of today's solved problems using script

---
+++ 2026-03-25/Target_Sum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        
+
+
+sol = Solution()
+print(sol.findTargetSumWays([1,1,1,1,1], 3))
