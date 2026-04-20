6e7ee2815743b21538bcce34189d294d959e0fb3
Lakshman Siva <slakshman664@gmail.com>
2026-04-20 17:00:06 -0400

    chore(leetcode): automated push of today's solved problems using script

---
+++ 2026-04-20/Reorganize_String.py
@@ -0,0 +1,35 @@
+import heapq
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        freq = Counter(s)
+        heap = []
+
+        for ch, count in freq.items():
+            heapq.heappush(heap, (-count, ch))
+
+        prev = None
+        res = []
+        while heap or prev:
+            if prev and not heap:
+                return ""
+
+            freq, ch = heapq.heappop(heap)
+            res.append(ch)
+            freq += 1
+
+            if prev:
+                heapq.heappush(heap, tuple(prev))
+                prev = None
+
+            if freq:
+                prev = [freq, ch]
+
+        return "".join(res)
+
+
+sol = Solution()
+print(sol.reorganizeString("aaab"))
