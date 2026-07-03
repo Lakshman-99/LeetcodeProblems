b370891146bc67be8bc91481226de9be46590b96
Lakshman Siva <slakshman664@gmail.com>
2026-07-03 17:00:04 -0400

    chore(leetcode): automated push of today's solved problems using script

--- 2026-07-03/Network_Recovery_Pathways.py
+++ 2026-07-03/Network_Recovery_Pathways.py
@@ -1,10 +1,35 @@
+import heapq
+import math
 from typing import List
 
 
 class Solution:
     def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
         n = len(online)
-        adj = [[] for _ in range()]
+        adj = [[] for _ in range(n)]
+
+        for u, v, c in edges:
+            adj[u].append([v, c])
+
+        dist = [math.inf] * n
+        dist[0] = 0
+
+        q = [(0, math.inf, 0)]
+        ans = -1
+        while q:
+            cost, mn, node = heapq.heappop(q)
+
+            if node == n-1:
+                ans = max(ans, mn)
+                continue
+
+            for nei, c in adj[node]:
+                nk = cost + c
+                if online[nei] and nk <= k and nk <= dist[nei]:
+                    dist[nei] = nk
+                    heapq.heappush(q, (nk, min(mn, c), nei))
+
+        return ans
 
 
 sol = Solution()
