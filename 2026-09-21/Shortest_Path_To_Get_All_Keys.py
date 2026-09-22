7a3f25ff9f60dd7da9931a662fff7badb1750ac7
Lakshman Siva <slakshman664@gmail.com>
2026-09-22 00:00:17 -0400

    chore(leetcode): automated push of today's solved problems using script

--- 2026-09-21/Shortest_Path_To_Get_All_Keys.py
+++ 2026-09-21/Shortest_Path_To_Get_All_Keys.py
@@ -4,16 +4,46 @@ from collections import deque
 class Solution:
     def shortestPathAllKeys(self, grid: list[str]) -> int:
         m, n = len(grid), len(grid[0])
-        
-        keys = 0
+
+        visited = set()
+        q = deque([])
+        target_mask = 0
         for i in range(m):
             for j in range(n):
-                if 'a' <= grid[i][j] <= 'z':
-                    keys += 1
+                cell = grid[i][j]
+                if 'a' <= cell <= 'z':
+                    target_mask |= 1 << (ord(cell) - ord('a'))
+                elif cell == '@':
+                    q.append((i, j, 0, 0))
+                    visited.add((i, j, 0))
 
-        grid[0][0] = "#"
+        DIRS = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        while q:
+            i, j, dist, key_mask = q.popleft()
 
+            if key_mask == target_mask:
+                return dist
+
+            for dr, dc in DIRS:
+                r, c = dr + i, dc + j
+                if 0 <= r < m and 0 <= c < n and grid[r][c] != "#":
+                    cell = grid[r][c]
+                    new_mask = key_mask
+                    if 'a' <= cell <= 'z':
+                        new_mask |= 1 << (ord(cell) - ord('a'))
+
+                    state = (r, c, new_mask)
+                    if state in visited:
+                        continue
+
+                    if 'A' <= cell <= 'Z' and not (new_mask & (1 << (ord(cell) - ord('A')))):
+                        continue
+
+                    visited.add(state)
+                    q.append((r, c, dist + 1, new_mask))
+
+        return -1
 
 
 sol = Solution()
-print(sol.shortestPathAllKeys(["@..aA","..B#.","....b"]))
+print(sol.shortestPathAllKeys(["@...a",".###A","b.BCc"]))
